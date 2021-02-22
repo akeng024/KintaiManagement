5327bf6859a67053f1eddc86080709e3ceefc2fa
akeng024 <a.k.eng024@gmail.com>
2021-02-23 02:12:31 +0900

    add: プロジェクトの追加

---
+++ kintai-management/src/main/java/com/akeng/kintaimanagement/KintaiManagementApplication.java
@@ -0,0 +1,13 @@
+package com.akeng.kintaimanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KintaiManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KintaiManagementApplication.class, args);
+	}
+
+}
---
+++ kintai-management/src/test/java/com/akeng/kintaimanagement/KintaiManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akeng.kintaimanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KintaiManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
