4581a9fd2ebc32276c93486e6e7aed480336947b
akeng024 <a.k.eng024@gmail.com>
2021-03-08 00:54:56 +0900

    fix: 不要な処理の削除

--- kintai-management/src/main/java/com/akeng/kintaimanagement/SecurityConfig.java
+++ kintai-management/src/main/java/com/akeng/kintaimanagement/SecurityConfig.java
@@ -1,31 +1,19 @@
 package com.akeng.kintaimanagement;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    UserDetailsService userDetailsService;
-
     @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
 
     /**
-     * 静的ファイルは認証しない
-     *
      * @param web
      * @throws Exception
      */
@@ -47,29 +35,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated();
         http.formLogin()
-                .loginProcessingUrl("/login") // ログイン処理
-                .loginPage("/login") // ログイン画面
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
                 .defaultSuccessUrl("/stamp")
-                .failureUrl("/login?error") // 認証失敗
-                //.successForwardUrl("/stamp") // 認証成功
-                .usernameParameter("email") // ユーザ
-                .passwordParameter("password"); // パスワード
+                .failureUrl("/login?error")
+                .usernameParameter("email")
+                .passwordParameter("password");
         http.logout()
-                //.logoutUrl("/logout")
-                .logoutSuccessUrl("/").permitAll(); // ログアウト成功
-        /*
-        http.authorizeRequests()
-                .mvcMatchers("/").permitAll()
-                .mvcMatchers("/admin/**").hasRole("ADMIN")
-                .anyRequest().authenticated()
-            .and()
-            .formLogin()
-                .defaultSuccessUrl("/")
-            .and()
-            .logout()
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID")
+                .logoutUrl("/logout")
                 .logoutSuccessUrl("/");
-         */
     }
 }
